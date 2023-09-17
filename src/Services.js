3687663b8c20de0d2cc307b9311b6231cfe355eb
Mohamed Baker <77456098+moebaker@users.noreply.github.com>
2023-09-17 04:20:43 -0400

    Add files via upload

---
+++ src/Services.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Services.css";
+
+function Services() {
+  return (
+
+    <div class="services">
+      
+      <div class="services-grid">
+                  
+                  <div class="service">
+                    
+                  </div>
+                  
+        </div>
+
+
+    </div>
+  )
+}
+
+export default Services
