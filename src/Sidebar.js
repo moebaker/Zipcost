024439ed2cda150cfc0ee9c72c87c9831dfad894
Mohamed Baker <77456098+moebaker@users.noreply.github.com>
2023-09-17 06:05:50 -0400

    Add files via upload

---
+++ src/Sidebar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Sidebar.css";
+
+
+
+function Sidebar() {
+    
+  return (
+    <div className="sidebar">
+
+        <div className="sidebar__top">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjcrFeTovYpISIXKPTtp_fiuFJb34avYyowA&usqp=CAU" alt=""/>
+            
+            <h2>{" "} ZipCost</h2>
+           
+            <h4>This project is all about empowering underrepresented minorities to find affordable communities that match their annual salary. 
+            We've developed a web app that calculates the cost of living in Miami for individuals, taking into account essential factors that determine the cost of living in any location.
+            In essence, our web app is a tool that goes beyond traditional cost-of-living calculators. It's a resource that empowers individuals, especially underrepresented minorities, to make informed decisions about their residential choices based on their unique financial situations. 
+            By calculating the cost of living and matching it with annual salaries, we're providing a valuable service that can positively impact lives.</h4>
+
+            
+            <h4> Cost of Living Calculation</h4>
+            <h4> User-Friendly Interface</h4>
+            <h4> Salary-Based Matching</h4>
+            <h4> Community Empowerment</h4>
+            <h4> Real-time Results</h4>
+            
+            
+            <h3> Our initiative aims to empower underrepresented minorities by giving them access to crucial information about living expenses. 
+            This enables them to make informed choices about their residential location.</h3>
+            <p> ZipCost inspired by Vanguard </p>
+                     
+            </div>
+
+    </div>
+  );
+}
+
+export default Sidebar
